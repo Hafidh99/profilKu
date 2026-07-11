2c8bcea7382f8b9c7056f4be67cf55f6a8a70e2e
Izumii~ <idlanahzadanish042015@gmail.com>
2026-07-11 18:26:46 +0700

    update

--- api/index.php
+++ api/index.php
@@ -1,3 +1,16 @@
 <?php
 
+$tmpDirs = [
+    '/tmp/storage/framework/views',
+    '/tmp/storage/framework/cache/data',
+    '/tmp/storage/framework/sessions',
+    '/tmp/storage/logs'
+];
+
+foreach ($tmpDirs as $dir) {
+    if (!is_dir($dir)) {
+        mkdir($dir, 0777, true);
+    }
+}
+
 require __DIR__ . '/../public/index.php';
